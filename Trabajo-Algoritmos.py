1a16a2955ae8591711b5154bcba53332cda3f92f
Liapesantez <lia.pesantez@est.ucacue.edu.ec>
2023-12-12 09:44:10 -0500

    a

---
+++ Trabajo-Algoritmos.py
@@ -0,0 +1,26 @@
+# Este programa permite realizar operaciones básicas, manipulación de cadenas de texto y conversión de tipos de datos
+
+# Ejercicio 1: : Operaciones Básicas y Tipos de Datos
+Suma = 5 + 4 # Realiza suma de números enteros
+Resta = 17.3 - 9.5 # Realiza resta de números decimales
+Multiplicacion = 23 * 3 # Realiza multiplicación de enteros
+Division = 42.18 / 11.2 # Realiza división de decimales
+
+print("la suma de 5 + 4 es", Suma)
+print("la resta de 17.3 - 9.5 es", Resta)
+print("la multiplicacion de 23 * 3 es", Multiplicacion)
+print("la division entre 42.18 / 11.2 es", Division)
+
+# Ejercicio 2: Manipulación de Cadenas de Texto
+nom = "LiaPesantez"
+long = len(nom) # Contabiliza la cantidad de caracteres
+print(nom, long)
+# El propósito de este programa es contabilizar la cantidad de caracteres que contiene una variable
+
+# Ejercicio 3: Conversión de Tipos de Datos
+a = 20
+b = 8.24
+c = float(a) # Transforma un número entero a decimal
+d = int(b) # Transforma un decimal a entero
+print(a, c)
+print(b, d)
